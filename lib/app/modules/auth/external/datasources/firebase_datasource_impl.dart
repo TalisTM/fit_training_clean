f0c449d26815d6d7c2b13912797b875f014e6f01
Talis <talismarchioro@gmail.com>
2022-07-30 21:49:21 -0300

    datasource

---
+++ lib/app/modules/auth/external/datasources/firebase_datasource_impl.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fit_training_clean/app/modules/auth/data/datasources/login_datasource.dart';
+import 'package:fit_training_clean/app/modules/auth/data/models/user_model.dart';
+import 'package:fit_training_clean/app/modules/auth/domain/errors/errors.dart';
+
+class FirebaseDatasourceImpl implements LoginDatasource {
+
+  final FirebaseAuth auth;
+  FirebaseDatasourceImpl(this.auth);
+
+  @override
+  Future<UserModel> loginEmail({required String email, required String password}) async {
+    var result = await auth.signInWithEmailAndPassword(email: email, password: password);
+    var user = result.user;
+
+    return UserModel(
+      name: user!.displayName ?? "",
+      email: user.email ?? "",
+      photoUrl: user.photoURL ?? "",
+      amountDone: 0,
+      restTimeInSeconds: 60,
+      workouts: []
+    );
+  }
+
+  @override
+  Future<UserModel> loginGoogle({required String idToken, required String accessToken}) async {
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      idToken: idToken, accessToken: accessToken
+    );
+    var result = await auth.signInWithCredential(credential);
+
+    var user = result.user;
+
+    return UserModel(
+      name: user!.displayName ?? "",
+      email: user.email ?? "",
+      photoUrl: user.photoURL ?? "",
+      amountDone: 0,
+      restTimeInSeconds: 60,
+      workouts: []
+    );
+  }
+
+  @override
+  Future<UserModel> currentUser() async {
+    var user = auth.currentUser;
+
+    if(user == null) throw ErrorGetLoggedUser();
+
+    return UserModel(
+      name: user.displayName ?? "",
+      email: user.email ?? "",
+      photoUrl: user.photoURL ?? "",
+      amountDone: 0,
+      restTimeInSeconds: 60,
+      workouts: []
+    );
+  }
+
+  @override
+  Future<void> logout() async {
+    return await auth.signOut();
+  }
+}
---
+++ test/app/modules/auth/external/datasources/firebase_datasource_impl_test.dart
@@ -0,0 +1,99 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fit_training_clean/app/modules/auth/domain/entities/user_entity.dart';
+import 'package:fit_training_clean/app/modules/auth/domain/errors/errors.dart';
+import 'package:fit_training_clean/app/modules/auth/external/datasources/firebase_datasource_impl.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class UserMock extends Mock implements User {}
+
+class UserCredentialMock extends Mock implements UserCredential {}
+
+class FirebaseAuthExceptionMock extends Mock implements FirebaseAuthException {}
+
+class FirebaseAuthMock extends Mock implements FirebaseAuth {}
+
+final credential = UserCredentialMock();
+final authException = FirebaseAuthExceptionMock();
+
+void main() {
+  final auth = FirebaseAuthMock();
+  final firebaseUser = UserMock();
+  final user = UserEntity(
+    name: "Talis",
+    email: "talismarchioro@gmail.com",
+    photoUrl: "",
+    amountDone: 0,
+    restTimeInSeconds: 60,
+    workouts: [],
+  );
+
+  final userCredential = UserCredentialMock();
+  final datasource = FirebaseDatasourceImpl(auth);
+
+  setUpAll(() {
+    when(() => firebaseUser.displayName).thenReturn("Talis");
+    when(() => firebaseUser.email).thenReturn("talismarchioro@gmail.com");
+    when(() => firebaseUser.photoURL).thenReturn("");
+    when(() => userCredential.user).thenReturn(firebaseUser);
+
+    when(
+      () => auth.signInWithEmailAndPassword(
+        email: any(named: "email"),
+        password: any(named: "password"),
+      ),
+    ).thenAnswer((_) async => userCredential);
+
+    registerFallbackValue(
+        const AuthCredential(providerId: "providerId", signInMethod: "signInMethod"));
+    when(() => auth.signInWithCredential(any())).thenAnswer((_) async => userCredential);
+  });
+
+  test("Deve retornar um UserEntity via loginEmail", () async {
+    var result = await datasource.loginEmail(email: "", password: "");
+
+    expect(result.name, equals(user.name));
+    expect(result.email, equals(user.email));
+    expect(result.photoUrl, equals(user.photoUrl));
+  });
+
+  test("Deve retornar um UserEntity via loginGoogle", () async {
+    var result = await datasource.loginGoogle(idToken: "", accessToken: "");
+
+    expect(result.name, equals(user.name));
+    expect(result.email, equals(user.email));
+    expect(result.photoUrl, equals(user.photoUrl));
+  });
+
+  test("Deve retornar um UserEntity via loggedUser", () async {
+    when(() => auth.currentUser).thenAnswer((_) => firebaseUser);
+
+    var result = await datasource.currentUser();
+
+    expect(result.name, equals(user.name));
+    expect(result.email, equals(user.email));
+    expect(result.photoUrl, equals(user.photoUrl));
+  });
+
+  test("Deve retornar um ErrorGetLoggedUser via loggedUser se não houver usuario logado", () async {
+    when(() => auth.currentUser).thenAnswer((_) => null);
+
+    var result = datasource.currentUser();
+
+    expect(result, throwsA(isA<ErrorGetLoggedUser>()));
+  });
+
+  test("Deve deslogar com sucesso via Logout", () {
+    when(() => auth.signOut()).thenAnswer((_) async {});
+
+    var result = datasource.logout();
+    expect(result, completes);
+  });
+
+  test("Deve returnar uma Exception via Logout", () {
+    when(() => auth.signOut()).thenThrow(Exception());
+
+    var result = datasource.logout();
+    expect(result, throwsA(isA<Exception>()));
+  });
+}
